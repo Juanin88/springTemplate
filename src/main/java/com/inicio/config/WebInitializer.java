7622d70cfb20902647b0945c357f5a852a55cf58
Juan Garfias Vázquez <ing.juan.garfias@gmail.com>
2018-07-18 17:05:17 -0500

    Spring+Tiles

--- src/main/java/com/inicio/config/WebInitializer.java
+++ src/main/java/com/inicio/config/WebInitializer.java
@@ -5,24 +5,46 @@
  */
 package com.inicio.config;
 
-import javax.servlet.ServletContext;  
-import javax.servlet.ServletException;  
-import javax.servlet.ServletRegistration.Dynamic;  
-  
-import org.springframework.web.WebApplicationInitializer;  
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
-import org.springframework.web.servlet.DispatcherServlet;  
-  
-public class WebInitializer implements WebApplicationInitializer {
-    
+//import javax.servlet.ServletContext;  
+//import javax.servlet.ServletException;  
+//import javax.servlet.ServletRegistration.Dynamic;  
+//  
+//import org.springframework.web.WebApplicationInitializer;  
+//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
+//import org.springframework.web.servlet.DispatcherServlet;  
+//  
+//public class WebInitializer implements WebApplicationInitializer {
+//    
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {        
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+//        ctx.register(Config.class);  
+//        ctx.setServletContext(servletContext);    
+//        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+//        servlet.addMapping("/");  
+//        servlet.setLoadOnStartup(1);
+//    }
+//    
+//}
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {        
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(Config.class);  
-        ctx.setServletContext(servletContext);    
-        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        servlet.addMapping("/");  
-        servlet.setLoadOnStartup(1);
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { Config.class };
     }
-    
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+ 
 }
