bd4956b0e8c57593230ad386299281326bf4c7d8
Juan Garfias Vázquez <ing.juan.garfias@gmail.com>
2018-07-23 15:24:52 -0500

    Ajustes

--- src/main/java/com/inicio/config/SpringMVCWebAppInitializer.java
+++ src/main/java/com/inicio/config/SpringMVCWebAppInitializer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.inicio.security.config.core;
+package com.inicio.config;
 
 import com.inicio.config.ApplicationConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
--- src/main/java/com/inicio/config/SpringSecurityInitializer.java
+++ src/main/java/com/inicio/config/SpringSecurityInitializer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.inicio.security.config.core;
+package com.inicio.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
